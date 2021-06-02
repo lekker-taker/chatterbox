ca0862194716df5cd8277d553290e260091fad8e
Dmytro Budnyk <dmitriy.budnik+catawiki@gmail.com>
2021-06-02 09:14:08 +0200

    Refactor import controller

--- app/controllers/api/v1/import.rb
+++ app/controllers/api/v1/import.rb
@@ -2,14 +2,16 @@ module API
   module V1
     # Exposes review import endpoint
     class Import < Grape::API
-      format :binary
-
       params { requires :reviews, type: File }
+
+      rescue_from JSON::ParserError do |e|
+        error!({error: {message: 'Invalid JSON'}}, 422)
+      end
 
       post "reviews" do
         ImportService.call(params.dig("reviews", "tempfile"))
-      rescue JSON::ParserError => e
-        error!({error: {message: e}}, 422)
+        status 201
+        body false
       end
     end
   end
