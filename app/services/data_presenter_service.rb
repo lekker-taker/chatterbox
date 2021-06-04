c620f3821464f3e33a0fc27daa7a66b24000d66a
Dmytro Budnyk <dmitriy.budnik+catawiki@gmail.com>
2021-06-04 19:16:33 +0200

    Adds presenter

---
+++ app/services/data_presenter_service.rb
@@ -0,0 +1,25 @@
+#  Converts ES agregation response to simple JSON
+#
+class DataPresenterService < ApplicationService
+  attr_reader :data
+
+  def initialize(data) # rubocop:disable Lint/MissingSuper
+    # Flattens nested aggreagation `filtered`
+    @data = data.dig("aggregations", "reviews", "filtered") || data.dig("aggregations", "reviews")
+  end
+
+  def call
+    return if data.nil?
+
+    {
+      by_theme: scores(:by_theme),
+      by_caterogy: scores(:by_caterogy)
+    }
+  end
+
+  private
+
+  def scores(type)
+    data.dig(type.to_s, "buckets").map{|theme| [theme["key"], theme.dig("average_score", "value")]}.to_h
+  end
+end
